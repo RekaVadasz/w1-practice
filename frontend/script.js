a17a8e9977e3674eeedbb285642a615757c9447c
RekaVadasz <vadasz.reka87@gmail.com>
2022-04-01 12:26:56 +0200

    practice on 04.01

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+console.log("Ez az első javascriptem")
+
+/*
+// a string legyen idézőjelek között 
+"ez itt egy szöveg"
+// a dupla és szimpla macskaköröm csak egysoros stringet enged
+'ez egy másik szöveg'
+
+`ez egy több
+soros string`
+
+4
+
+128
+
+04
+
+1.13
+*/
+function myFirstFunction(){
+    console.log("ez az első függvényem belsejében levő log")
+}
+
+myFirstFunction()
